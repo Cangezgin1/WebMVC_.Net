2b994e7a46f470535380f8ce0cbda95f43f25931
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-11-25 17:44:47 +0300

    Değişiklikler

---
+++ BusinessLayer/ValidationRules/WriterValidator2.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator2 : AbstractValidator<Writer>
+    {
+        public WriterValidator2()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz.");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadını boş geçemezsiniz.");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar mailini boş geçemezsiniz.");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Yazar şifresini boş geçemezsiniz.");
+        }
+    }
+}
---
+++ WebMVC/Controllers/RegisterController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccsesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        WriterValidator2 writervalidator = new WriterValidator2();
+
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult KayıtOl()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KayıtOl(Writer p)
+        {
+            //ValidationResult result = writervalidator.Validate(p);
+            //if (result.IsValid)
+            //{
+                wm.WriterAdd(p);
+                return RedirectToAction("WriterLogin","Login");
+            //}
+            //else
+            //{
+            //    foreach (var item in result.Errors)
+            //    {
+            //        ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            //    }
+            //}
+            //return View();
+        }
+    }
+}
